165f77320348848f8512d6450fd3e969514b7d10
Natho74 <njbenton@gmail.com>
2023-02-04 11:24:10 -0600

    add fileReader
    run it for pretty world

--- core/src/com/mygdx/rootsgame/GameWorld.java
+++ core/src/com/mygdx/rootsgame/GameWorld.java
@@ -22,14 +22,15 @@ public class GameWorld {
 	//populates the game world with tiles
 	public void initialize() {
 		//TODO: go through GameWorld and set them to objects
+		String worldSeed = Reader.readStringFromFile("test.txt");
 		for(int y = 0; y < worldSizeY; y++) {
 			for(int x = 0; x < worldSizeX; x++) {
 				if(x % 2 == 0) {
-					World[x][y] = new Soil();
+					World[x][y] = getTileFromChar(worldSeed.charAt(x + (y*worldSizeX)));
 					World[x][y].SetGridLocation(x, y);
 				}
 				else {
-					World[x][y] = new Water();
+					World[x][y] = getTileFromChar(worldSeed.charAt(x + (y*worldSizeX)));
 					World[x][y].SetGridLocation(x, y);
 				}
 			}
@@ -37,6 +38,17 @@ public class GameWorld {
 		initialized = true;
 	}
 	
+	private Tile getTileFromChar(char tileChar) {
+		if(tileChar == 'S')
+			return new Soil();
+		if(tileChar == 'W')
+			return new Water();
+		if(tileChar == 'G')
+			return new Grass();
+		else
+			return new Soil();
+	}
+
 	//Advances the world clock by one tick
 	public void advanceClock() {
 		clock++;
